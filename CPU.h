6067c356ba58cb13e9a9c2c58552a7d8a38ce7ee
Carlos Peñas <carlos.penas.n@gmail.com>
2021-09-05 10:34:21 +0200

    remove auto generated comments

--- CPU.h
+++ CPU.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   CPU.h
- * Author: carlos
- *
- * Created on September 4, 2021, 10:19 AM
- */
-
 #ifndef CPU_H
 #define CPU_H
 
